efdc112d4da1421aace9a54a6255decaf9c7145c
Maximilian Kratz <account@maxkratz.com>
2025-04-13 09:55:43 +0200

    Snapshot

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@
 set -e
 
 # Define the curl binary path and parameters
-CURL="/usr/bin/curl -f -S -s"
+# CURL="/usr/bin/curl -f -S -s"
+CURL="curl -f -S -s"
 
 # Check arguments
 if [[ -z "$1" ]]; then
@@ -21,10 +22,13 @@ TARGET_REPO="maxkratz/docker_texlive"
 
 # Get all releases from the GitHub API
 get_releases () {
-    release_json=$($CURL -L \
-        -H "Accept: application/vnd.github+json" \
-        -H "X-GitHub-Api-Version: 2022-11-28" \
-        https://api.github.com/repos/$REPO/releases)
+    # release_json=$($CURL -L \
+    #     -H "Accept: application/vnd.github+json" \
+    #     -H "X-GitHub-Api-Version: 2022-11-28" \
+    #     https://api.github.com/repos/$REPO/releases)
+
+    release_json=$(cat release.json)
+
     # echo $release_json
     names=$(echo $release_json | jq -r .[].name)
     publish_dates=$(echo $release_json | jq -r .[].published_at)
@@ -55,61 +59,88 @@ get_issues () {
 
   # TODO: ^while loop (with an upper limit ...) that exits when a request does not return any issues.
 
-  declare -a titles=(  )
+  # ids=(  )
+  titles=(  )
 
   i=0
-  for limit in $(seq 1 33334);
+  # for limit in $(seq 1 33334);
+  for limit in $(seq 1 1);
+  # TODO: fix limit i.e. curl error code 422
   do
-      issues_i_json=$($CURL -L \
-        -H "Accept: application/vnd.github+json" \
-        -H "X-GitHub-Api-Version: 2022-11-28" \
-        https://api.github.com/repos/$TARGET_REPO/issues\?state\=all\&page\=$i)
+      # issues_i_json=$($CURL -L \
+      #   -H "Accept: application/vnd.github+json" \
+      #   -H "X-GitHub-Api-Version: 2022-11-28" \
+      #   "https://api.github.com/repos/$TARGET_REPO/issues?state=all&page=$i")
+      issues_i_json=$(cat test.json)
+
+      #echo $TARGET_REPO
+      #echo $issues_i_json
+
+      if [ "$?" -gt 0 ]; then
+        break;
+      fi
 
       if [ -z "${issues_i_json}" ]; then
         break;
       fi
 
-      # echo $issue_i_json
-      titles_i=$(echo $issue_i_json | jq -r .[].title)
+      # ids_i=$(echo $issues_i_json | jq -r .[].id)
+      titles_i=$(echo $issues_i_json | jq -r .[].title)
+      # echo "$ids_i"
+      # echo "$titles_i"
 
-      # echo $titles_i
+      # echo $issues_i_json | jq -r .[].title
+
 
       IFS_ORIG=$IFS  # Save current IFS (Internal Field Separator)
       IFS=$'\n'      # Change IFS to newline char
-      titles_i=($titles_i) # split the `titles` string into an array by the same name
+      titles_i=($titles_i) # split the `names` string into an array by the same name
       IFS=$IFS_ORIG  # Restore original IFS
 
-      echo ${titles_i[@]}
 
-      titles+=(${titles_i[@]})
+      # ids+=(${ids_i[@]})
+      titles+=("${titles_i[@]}")
+      # for (( j=0; j<${#titles_i[@]}; j++ ));
+      # do
+      #   titles+=("${titles_i[$j]}")
+      # done
+      # ids+=($(echo $issues_i_json | jq -r .[].title))
       i+=1
   done
 }
 
-get_issues
+# get_issues
 
-for (( i=0; i<${#titles[@]}; i++ ));
-do
-  echo "index: $i, title: ${titles[$i]}"
-done
+# # for (( i=0; i<${#ids[@]}; i++ ));
+# # do
+# #   echo "index: $i, id: ${ids[$i]}"
+# # done
 
-exit 0
+# for (( i=0; i<${#titles[@]}; i++ ));
+# do
+#   echo "index: $i, title: ${titles[$i]}"
+# done
+
+# exit 0
 
 #
 # Script
 #
 
 get_releases
+get_issues
 set_start_epoch
 
 # For all found releases ...
+relevant_release_ids=(  )
 for (( i=0; i<${#publish_dates[@]}; i++ ));
 do
   publish_epoch=$(date_string_to_epoch ${publish_dates[$i]})
 
   # ... check if a particular release was after the start date
   if [ "$start_epoch" -lt "$publish_epoch" ]; then
-    echo "index: $i, value: $publish_epoch"
+    # echo "index: $i, value: $publish_epoch"
+    relevant_release_ids+=($i)
   fi
 done
 
@@ -117,6 +148,11 @@ done
 # - get all issues of the target GitHub repository
 # - check each matched release from above -> is there already an issue?
 # - for all other releases -> create a new issue per release
+
+for (( i=0; i<${#relevant_release_ids[@]}; i++ ));
+do
+  echo "index: $i, title: ${relevant_release_ids[$i]}"
+done
 
 # TODO
 exit 0
