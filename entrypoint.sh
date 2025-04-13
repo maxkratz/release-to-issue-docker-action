f3a6122ec8ff38e07cbb0686f437e3adf1fbb290
Maximilian Kratz <account@maxkratz.com>
2025-04-13 13:25:41 +0200

    Remove tmp file after run + clean up in entrypoint.sh script

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,10 @@ SOURCE_REPO=$1
 TARGET_REPO=$2
 START_DATE=$3
 ASSIGNEE=$4
-
 GITHUB_USERNAME=$5
 GITHUB_TOKEN=$6
 
-# Create temp file with GitHub credentials
+# Create tmp file with GitHub credentials
 echo "login=$GITHUB_USERNAME" > /github.properties
 echo "password=$GITHUB_TOKEN" > /github.properties
 
@@ -20,4 +19,7 @@ JAR=release-to-issue-java-*-jar-with-dependencies.jar
 
 # Run Java program
 java -jar $JAR --sourcerepo $SOURCE_REPO --targetrepo $TARGET_REPO --datelimit $START_DATE --assignee $ASSIGNEE --dryrun
+
+# Delete tmp file with GitHub credentials
+rm -f /github.properties
 exit 0
